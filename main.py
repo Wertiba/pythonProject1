c4b624827784c43b9a05c43ff0e9e220be52d703
wertiba <wertibarozen@gmail.com>
2024-08-14 12:32:56 +0300

    рефакторинг

--- main.py
+++ main.py
@@ -6,43 +6,42 @@ import re
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 #подключение фото
-img = cv2.imread('photos/photo_2024-08-14_12-04-06.jpg')
+img = cv2.imread('photos/photo_2024-08-14_12-23-56.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-#будет выведен весь текст с картинки
+#получение текста с картинки
 config = r'--oem 3 --psm 6'
 text = (pytesseract.image_to_string(img, config=config, lang='rus+eng'))
 
 #координаты слов
 data = pytesseract.image_to_data(img, config=config, lang='rus')
-def find_word():
-    tags = re.finditer(r"кодовое слово (\S+)", text)
-    # for tag in tags:
-    #     word = tag.group(1)
-    #
-    #     return word
-    list = [tag.group(1) for tag in tags]
-    return list
+
+#нахождение секретного слова
+tags = re.finditer(r"кодовое слово (\S+)", text)
+secret_words = [tag.group(1) for tag in tags]
 
 
-for i, el in enumerate(data.splitlines()):
-    if i == 0:
-        continue
+def refactor():
+    for i, el in enumerate(data.splitlines()):
+        #скип первой итерации
+        if i == 0:
+            continue
 
-    el = el.split()
+        el = el.split()
 
-    try:
-        x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
+        try:
+            x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
 
-        for word in find_word():
-            if el[11] == word:
-                cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 0), thickness=-1)
+            for word in secret_words:
+                if el[11] == word:
+                    cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 0), thickness=-1)
 
-    except IndexError:
-        # print('Операция пропущена')
-        pass
+        except IndexError:
+            # print('Операция пропущена')
+            pass
 
 
-
-cv2.imshow('result', img)
-cv2.waitKey(0)
+if __name__ == '__main__':
+    refactor()
+    cv2.imshow('result', img)
+    cv2.waitKey(0)
