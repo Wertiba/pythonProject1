36bdf16e7e3de2b81f11ce5f14e4d00b8c8594c1
wertiba <wertibarozen@gmail.com>
2024-08-21 14:14:45 +0300

    add logging and fix bug with —

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import cv2
 import pytesseract
 import re
 
+from loguru import logger
+
 #tesseract connection
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 config = r'--oem 3 --psm 6'
 
 #photo connection
-img = cv2.imread('photos/photo_2024-08-20_15-52-37.jpg')
+img = cv2.imread('photos/photo_2024-08-20_15-49-35.jpg')
+
+#loguru setting
+#logger.add('debug.log', format='{time} {level} {message}', level='DEBUG', rotation='10 KB', compression='zip')
 
 
 def refactor(img):
@@ -15,8 +20,8 @@ def refactor(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     #getting text from image
-    text = (pytesseract.image_to_string(img, config=config, lang='rus+eng'))
-    print(text)
+    text = (pytesseract.image_to_string(img, config=config, lang='rus'))
+    logger.info(text)
 
     # coordinates of words
     data = pytesseract.image_to_data(img, config=config, lang='rus')
@@ -25,7 +30,7 @@ def refactor(img):
     tags = re.finditer(
         r'(ко)(.?)(-|—?)(.?)(\s*)(.?)(до)(.?)(-|—?)(.?)(\s*)(.?)(во)(.?)(-|—?)(\s*)(.?)(е)(.?)(\s)(.?)(сло)(.?)(-|—?)(.?)(\s*)(.?)(во)(.?)(\s*)([а-яА-Яa-zA-Z\-_]+)(\.?)(\n?)', text)
     secret_words = [tag for tag in tags]
-    print(secret_words)
+    logger.info(secret_words)
 
 
     for i, el in enumerate(data.splitlines()):
@@ -42,7 +47,7 @@ def refactor(img):
             for symbol in symbols_to_remove:
                 el[11] = el[11].replace(symbol, "")
 
-            print(el[11])
+            logger.info(el[11])
 
 
             if again == True:
@@ -53,7 +58,7 @@ def refactor(img):
                 if str(el[11]).lower() == str(word.group(31)).lower():
                     cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 0), thickness=-1)
 
-                    if str(el[11]).lower()[-1] == '-':
+                    if str(el[11]).lower()[-1] == '-' or '—':
                         again = True
 
                     if str(word.group(33)) == '\n':
@@ -61,8 +66,7 @@ def refactor(img):
 
 
         except IndexError:
-            # print('Операция пропущена')
-            pass
+            logger.warning('Операция пропущена')
 
     return img
 
